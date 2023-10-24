5a8ad897686a9a7838dce1b994b5db07dddef464
jatin mahajan <jatinmahajan711@gmail.com>
2023-10-24 18:38:02 -0400

    update setup file

--- .deploy/packer/setup.sh
+++ .deploy/packer/setup.sh
@@ -1,37 +1,29 @@
+#!/bin/bash
+
+# tell bash to print out the statements as they are being executed so that we can see them running
 set -x
 
-sudo apt-get update
-
+# Install necessary dependencies - install docker - first run update - comments
+sudo apt-get update -y 
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
     lsb-release -y
-
+# Add Docker’s official GPG key:
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
+# Use the following command to set up the stable repository
 echo \
-"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# install docker 
+sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo chmod 666 /var/run/docker.sock
-
+# verify the installation by running hello-world image as container
 sudo docker run hello-world
-sudo docker login -username $DOCKER_USERNAME -password $DOCKER_PASSWORD
+# login to docker and pull down image
+sudo docker login --username $USERNAME --password $PASSWORD
 docker pull johnny711dock/terra-project-api:latest
 
-sudo groupadd -r hashicorp
-sudo usermod -m -g /bin/bash terraform
-sudo usermod -a -G hashicorp terraform
-sudo cp /etc/sudoers /etc/sudoers.orig
-echo "terraform ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/terraform
-
-sudo mkdir -p /home/terraform/.ssh
-sudo chmod 700 /home/terraform/.ssh
-sudo cp /tmp/tf.pub /home/terraform/.ssh/authorized_keys
-sudo chmod 600 /home/terraform/.ssh/authorized_keys
-sudo chown -R terraform /home/terraform/.ssh
-sudo usermod --shell /bin/bash terraform
