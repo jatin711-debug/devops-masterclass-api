bce511de938857d0726824cef7791d2c72941204
jatin mahajan <jatinmahajan711@gmail.com>
2023-10-24 19:12:45 -0400

    COMMIT MSSG

--- .deploy/packer/setup.sh
+++ .deploy/packer/setup.sh
@@ -7,26 +7,17 @@ set -x
 sudo apt-get update -y 
 # Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt-get install \
+    software-properties-common \
     apt-transport-https \
     dialog apt-utils \
     ca-certificates \
     curl \
     gnupg \
+    snapd \
     lsb-release -y
-# Add Docker’s official GPG key:
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo snap install docker
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# verify the installation by running hello-world image as container
 sudo docker run hello-world
 # login to docker and pull down image
 sudo docker login --username $USERNAME --password $PASSWORD
