696b64b8416bfaaf97197a29e5f4857ab0f13e52
jatin mahajan <jatinmahajan711@gmail.com>
2023-10-24 18:48:12 -0400

    commit mssg

--- .deploy/packer/setup.sh
+++ .deploy/packer/setup.sh
@@ -22,7 +22,9 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # verify the installation by running hello-world image as container
 sudo docker run hello-world
 # login to docker and pull down image
