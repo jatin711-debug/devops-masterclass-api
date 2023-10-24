d80a63ae2faff522101c8a4ec4ecabdf7c1113b0
jatin mahajan <jatinmahajan711@gmail.com>
2023-10-24 19:29:11 -0400

    commit

--- .deploy/packer/setup.sh
+++ .deploy/packer/setup.sh
@@ -6,12 +6,12 @@ set -x
 # Install necessary dependencies - install docker - first run update - comments
 sudo apt-get update -y 
 # Update the apt package index and install packages to allow apt to use a repository over HTTPS:
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt-cache policy docker-ce
+sudo apt-get install docker-ce
+docker --version
 sudo docker run hello-world
 # login to docker and pull down image
 sudo docker login --username $USERNAME --password $PASSWORD
